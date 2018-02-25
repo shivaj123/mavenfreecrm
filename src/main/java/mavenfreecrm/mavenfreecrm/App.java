89c4f583bb65c1a0faa58b97cd4a321abacb03be
shivaji <shivaji@hp>
2018-02-25 22:10:50 +0530

    added freecrm project

---
+++ src/main/java/mavenfreecrm/mavenfreecrm/App.java
@@ -0,0 +1,13 @@
+package mavenfreecrm.mavenfreecrm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
